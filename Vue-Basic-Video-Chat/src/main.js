fb17dd2dbcea05aa09cd0179a276b5531a158f2a
CleverStarCodes <coder.genius07@gmail.com>
2018-05-02 00:20:28 +0300

    first commit

---
+++ Vue-Basic-Video-Chat/src/main.js
@@ -0,0 +1,31 @@
+// Import promise and fetch polyfills for IE11
+import 'promise-polyfill/src/polyfill';
+import 'whatwg-fetch';
+
+import Vue from 'vue';
+import Session from './Session.vue';
+
+const createSession = (apiKey, sessionId, token)=> {
+  new Vue({
+    el: '#app',
+    render: h => h(Session, {
+      props: {
+        sessionId,
+        apiKey,
+        token
+      }
+    })
+  });
+};
+
+if (config.API_KEY && config.TOKEN && config.SESSION_ID) {
+  createSession(config.API_KEY, config.SESSION_ID, config.TOKEN);
+} else {
+  fetch(config.SAMPLE_SERVER_BASE_URL + '/session')
+    .then((data) => data.json())
+    .then((json) => {
+      createSession(json.apiKey, json.sessionId, json.token);
+    }).catch((err) => {
+      alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
+    });
+}
